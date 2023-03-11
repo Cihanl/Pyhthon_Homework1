31ce7829f2541773cacd49f9b78dd17023e06288
Dünyaa <86017820+Cihanl@users.noreply.github.com>
2023-03-11 20:37:19 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,27 @@
+# 1. Veri Tipi Çeşitleri 
+  # Metin tipleri: str  
+    # örn:   x = "Merhaba Dünya"
+  # Sayısal tipler: int , float , complex 
+    # örn : x = 20 (int) -  x = 20.5(float)  -   x = 1j(complex)
+  # Dizi tipleri:  list , tuple , range  
+    # örn : x = ["elma", "muz", "çilek"]  (list) - x = ("elma", "muz", "çilek") (tuple) - x = range(6)
+  # Adresleme tipleri: dict  
+    # örn : x = {"isim" : "Yunus", "yas" : 36} 
+  # Küme tipleri: set , frozenset  
+    # örn : x = {"elma", "muz", "çilek"} (set) - x = frozenset({"elma", "muz", "çilek"})  (frozenset)
+  # Mantıksal tipler: bool  
+    # örn : x = True
+  # Binary tipler: bytes , bytearray , memoryview  
+    # örn : x = b"Merhaba" (bytes) - x = bytearray(5) (bytearray) - x = memoryview(bytes(5)) 
+
+# 2. Kodlama.io'daki veri örnekleri:
+  # Sitedeki yazıların tamamı string olarak ifade edilebilir; "Ödev tanımı, Amaç vs."
+
+# 3.
+
+# bitirVeDevamEt = TRUE
+# if bitirVeDevamEt == TRUE:
+  #  print("Bir sonraki derse gelebilir.")
+
+# else:
+# print("Lütfen dersi kontrol ediniz")
